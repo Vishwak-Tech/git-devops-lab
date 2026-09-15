f3e3824d8e5fcf628c920475e12ada4bda8ab917
Vishwak Koleti <koletivishwak84@gmail.com>
2026-09-15 06:19:05 +0000

    add docker build to ci pipeline

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ function isEven(number) {
   return number % 2 === 0;
 }
 
+if (require.main === module) {
+  console.log("Git DevOps Lab is running inside Docker!");
+  console.log("2 + 3 =", add(2, 3));
+  console.log("4 × 3 =", multiply(4, 3));
+  console.log("Is 10 even?", isEven(10));
+}
+
 module.exports = {
   add,
   subtract,
